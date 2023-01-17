d48720161894134b00e3dac861979354295027e9
Danijel Lapadatovic <l.danijel977@gmail.com>
2023-01-17 17:20:19 +0100

    Created and displayed first post

---
+++ lesson_02/src/App.js
@@ -0,0 +1,11 @@
+import { useState } from "react"; 
+import PostsList from "./features/posts/PostsList";
+function App() {
+  return (
+    <main className="App">
+      <PostsList/>
+    </main>
+  );
+}
+
+export default App;
